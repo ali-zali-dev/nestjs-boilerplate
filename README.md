edd545a2f05c7c3a19f523f0a1b7a085a469d107
Ali Zali <ali.zali.dev@gmail.com>
2021-11-23 12:51:53 +0330

    feat(config): add swagger to project

--- README.md
+++ README.md
@@ -71,3 +71,11 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+## TODOs
+
+- Initialize project
+- Add swagger
+- Add typeorm and config for postgresql
+- Add auth module
+- Add user module
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Nestjs boilerplate')
+      .setDescription('The nestjs boilerplate description')
+      .setVersion('1.0')
+      .addTag('nestjs')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
+
   await app
     .listen(process.env.SERVICE_PORT, process.env.SERVICE_HOST)
     .then(() => {
