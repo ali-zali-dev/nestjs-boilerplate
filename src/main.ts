0551ba2a04a3fa2ef0f9e26b348e97d6fce6bdf2
Ali Zali <ali.zali.dev@gmail.com>
2021-11-30 13:54:13 +0330

    feat(main): add explorer to swagger

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document, {
+      explorer: true,
       swaggerOptions: {
         persistAuthorization: true,
       },
