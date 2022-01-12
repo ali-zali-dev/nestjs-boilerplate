c62ef0fae798cdd21f7487d9d7d5038abebab3f0
Ali Zali <ali.zali.dev@gmail.com>
2022-01-12 12:05:18 +0330

    feat: create chat, chatroom and chat-socket module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { ChatroomModule } from './chatroom/chatroom.module';
+import { ChatSocketModule } from './chat-socket/chat-socket.module';
 
 @Module({
   imports: [
@@ -13,6 +16,9 @@ import { AppService } from './app.service';
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`,
     ),
+    ChatModule,
+    ChatroomModule,
+    ChatSocketModule,
   ],
   controllers: [AppController],
   providers: [AppService],
